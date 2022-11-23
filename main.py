48f30bcda2127c815a6e8f60436c507135b476ea
Daniel Chaves <danielchaves@uepg.br>
2022-11-23 16:24:11 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from os import link
+from tkinter import *
+from editorSaxena import TextEditor
+from afd import *
+
+if __name__ == '__main__':
+    root = Tk()
+    TextEditor(root)
+    root.mainloop()
